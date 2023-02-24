3f7f6a963affccd40693b6c862040268c748aecb
Leonel <marinaro.leonel@tecnica7.edu.ar>
2023-02-24 11:10:52 -0300

    modify main.js and indexs.html > add content class with js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,21 +1,17 @@
-const API =
-  "https://youtube-v31.p.rapidapi.com/search?channelId=UC8LeXCWOalN8SxlrPcG-PaQ&part=snippet%2Cid&order=date&maxResults=5";
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
 
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    "X-RapidAPI-Key": "1877404507msh7617bcccc6c94ebp1e7291jsna83b1d65036a",
-    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-  },
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+  }
 };
 
-// fetch(API, options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
 async function fetchData(urlApi) {
-  const response = await fetch(urlAPI, options);
+  const response = await fetch(urlApi, options);
   const data = await response.json();
   return data;
 }
@@ -23,5 +19,24 @@ async function fetchData(urlApi) {
 (async () => {
   try {
     const videos = await fetchData(API);
-  } catch {}
+    let view = `
+    ${videos.items.map(video => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(0, 4).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
 })();
